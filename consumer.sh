014625a6b92fa7506aa331b96511dd0be8c6173c
siangyeh8818 <siangyeh8818@gmail.com>
2019-03-13 11:54:05 +0800

    adding namespace and try in Canary env

--- consumer.sh
+++ consumer.sh
@@ -1,4 +1,4 @@
-kubectl exec -ti kafka-test-client -- \
-/usr/bin/kafka-console-consumer --bootstrap-server kafka:9092 \
+kubectl exec -ti kafka-test-client -n monitor -- \
+/usr/bin/kafka-console-consumer --bootstrap-server kafka-svc:9092 \
 --topic test1 --from-beginning
 
--- publisher.sh
+++ publisher.sh
@@ -2,5 +2,5 @@
 #       /usr/bin/kafka-topics --interactive  \
 #        --topic test \
 #        --zookeeper kafka:9092
-kubectl exec -it   kafka-test-client -- \
-        /usr/bin/kafka-console-producer  --topic test1 --broker-list kafka:9092 
+kubectl exec -it   kafka-test-client -n monitor -- \
+        /usr/bin/kafka-console-producer  --topic test1 --broker-list kafka-svc:9092 
